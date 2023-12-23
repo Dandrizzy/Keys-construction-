eb765d51a9193eac10a5e46dce028b84bd565240
unknown <dandrizzy4all@gmail.com>
2023-12-23 04:55:28 -0800

    fourth

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -6,22 +6,27 @@ import Heading from "../ui/Heading";
 const LoginLayout = styled.main`
   min-height: 100vh;
   display: grid;
-  grid-template-columns: 48rem;
+  grid-template-columns: clamp(48rem) ;
   align-content: center;
   justify-content: center;
   gap: 3.2rem;
-  backdrop-filter: blur(45px);
+  padding: 0 2rem;
+  background: rgba(0 0 0 / 0.2);
+  /* backdrop-filter: grayscale(200px); */
 
 `;
 
 function Login() {
- return (
-  <LoginLayout id="login">
-   {/* <Logo /> */}
-   <Heading as="h4">Log in to your account</Heading>
-   <LoginForm />
-  </LoginLayout>
- );
+  return (
+    <div className=" bg-rose-800 text-slate-100 ">
+      <LoginLayout id="login">
+        {/* <Logo /> */}
+        <Heading as="h4">Log in to your account</Heading>
+        <LoginForm />
+
+      </LoginLayout>
+    </div>
+  );
 }
 
 export default Login;
